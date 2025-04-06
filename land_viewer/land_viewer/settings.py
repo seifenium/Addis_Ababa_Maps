2c45ede696cb3c637547760aa3edc7aaa1bec040
Seife Asnake <seifenium@gmail.com>
2025-04-06 12:51:14 +0200

    Update settings.py

--- land_viewer/land_viewer/settings.py
+++ land_viewer/land_viewer/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_URL = '/static/'
 
 # Add this if not present
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
